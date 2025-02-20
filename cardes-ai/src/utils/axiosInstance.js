14825938d4a819a400cda54d328d80d5c6eee3b6
Raul <123974722+3raulo9@users.noreply.github.com>
2025-02-21 01:31:42 +0200

    Update axiosInstance.js

--- cardes-ai/src/utils/axiosInstance.js
+++ cardes-ai/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000", // Django backend URL
+  baseURL: "https://cardes-ai.onrender.com", // Django backend URL
   headers: {
     "Content-Type": "application/json",
   },
