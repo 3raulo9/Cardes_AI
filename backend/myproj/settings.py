7355340e571772001d23ea22477193a7aa4bc91e
Raul <123974722+3raulo9@users.noreply.github.com>
2025-03-10 18:21:41 +0200

    secret key in env

--- backend/myproj/settings.py
+++ backend/myproj/settings.py
@@ -8,11 +8,9 @@ from django.core.cache import cache
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ua!%&yniddhy_9d72rb#cv*$b!!f-8o^upf+t0pkv$%%fjid67'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # ❌ Avoid logging sensitive data
