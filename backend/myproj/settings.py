312646022577ad89887b5c6e642cb012ab529e95
Raul <123974722+3raulo9@users.noreply.github.com>
2025-03-23 16:23:58 +0200

    new db
    now using postgres instead of sqlite and using service called neon for storing the db

--- backend/myproj/settings.py
+++ backend/myproj/settings.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import logging
 from datetime import timedelta
 from django.core.cache import cache
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,12 +146,10 @@ WSGI_APPLICATION = "myproj.wsgi.application"
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
